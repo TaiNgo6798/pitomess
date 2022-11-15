fa95adaddb4935afc3174373b05d413fa289f0d6
Tai Ngo <taingo6798@gmail.com>
2022-11-15 15:24:12 +0700

    test routing

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -2,31 +2,31 @@ const
   config = require('config'),
   request = require('request');
 
-// App Secret can be retrieved from the App Dashboard
-const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
-  process.env.MESSENGER_APP_SECRET :
-  config.get('appSecret');
+// // App Secret can be retrieved from the App Dashboard
+// const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
+//   process.env.MESSENGER_APP_SECRET :
+//   config.get('appSecret');
 
-// Arbitrary value used to validate a webhook
-const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
-  (process.env.MESSENGER_VALIDATION_TOKEN) :
-  config.get('validationToken');
+// // Arbitrary value used to validate a webhook
+// const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
+//   (process.env.MESSENGER_VALIDATION_TOKEN) :
+//   config.get('validationToken');
 
-// Generate a page access token for your page from the App Dashboard
-const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
-  (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
-  config.get('pageAccessToken');
+// // Generate a page access token for your page from the App Dashboard
+// const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
+//   (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
+//   config.get('pageAccessToken');
 
-// URL where the app is running (include protocol). Used to point to scripts and 
-// assets located at this address. 
-const SERVER_URL = (process.env.SERVER_URL) ?
-  (process.env.SERVER_URL) :
-  config.get('serverURL');
+// // URL where the app is running (include protocol). Used to point to scripts and 
+// // assets located at this address. 
+// const SERVER_URL = (process.env.SERVER_URL) ?
+//   (process.env.SERVER_URL) :
+//   config.get('serverURL');
 
-if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
-  console.error("Missing config values");
-  process.exit(1);
-}
+// if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
+//   console.error("Missing config values");
+//   process.exit(1);
+// }
 
 export default function handler(req, res) {
   try {
