aef3fa065ed4321574d308f7ab69ce3c0438dd55
Tai Ngo <taingo6798@gmail.com>
2022-11-15 14:31:47 +0700

    test routing

--- api/index.js
+++ api/index.js
@@ -72,6 +72,10 @@ app.get('/webhook', function (req, res) {
   }
 });
 
+app.get('/webhook', function (req, res) {
+  res.status(200).send("from index.js");
+});
+
 /*
  * All callbacks for Messenger are POST-ed. They will be sent to the same
  * webhook. Be sure to subscribe your app to your page to receive callbacks
