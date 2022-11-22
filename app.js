a6c22d55177570077cce842cd2dd29485ee46ed0
Tai Ngo <taingo6798@gmail.com>
2022-11-22 13:04:55 +0700

    test

--- app.js
+++ app.js
@@ -334,6 +334,7 @@ function callSendAPI(messageData) {
 // certificate authority.
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
+  sendTextMessage(config.get('myInboxId'), "Deplyed!")
 });
 
 module.exports = app;
